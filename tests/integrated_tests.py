d53a63188be6012bb0704b363d05d447ae788ef3
James Davis <jadinvt@gmail.com>
2012-07-26 12:54:20 -0500

    Started adding wiki tests--found hole in wiki post already

--- tests/integrated_tests.py
+++ tests/integrated_tests.py
@@ -7,7 +7,12 @@ from urllib2 import urlopen
 from BeautifulSoup import BeautifulSoup
 
 base_url = "http://localhost:8080"
+login_path = "/login"
+login_url = base_url+login_path
 blog_path = "/blog"
+wiki_path ="/wiki"
+edit_path = "/_edit"
+history_path = "/_history"
 blog_new_page = blog_path + "/newpost"
 redirect_codes = (300, 301, 302, 303, 307)
 
@@ -31,7 +36,7 @@ class BlogInterfaceTests(unittest.TestCase):
             print "Tearing down"
             
     def get_random_string(self, length=50):
-        return ''.join(random.choice(string.letters) for i in xrange(100))
+        return ''.join(random.choice(string.letters) for i in xrange(length))
     
     def dummy_test(self):        
         self.assertTrue(True)
@@ -63,4 +68,26 @@ class BlogInterfaceTests(unittest.TestCase):
         
         self.assertGreater(soup.text.rfind(subject), -1)
         self.assertGreater(soup.text.rfind(content), -1)
-        
+         
+    def wiki_new_page_redirect_test(self):    
+        subject = self.get_random_string(length=10)
+        opener = urllib2.build_opener(RedirectCatchHandler())
+        page = opener.open(base_url+wiki_path+"/%s"%subject)
+        self.assertTrue(page.getcode() in redirect_codes)
+        if settings.DEBUG:
+            print "redirct to %s"%(page.geturl())
+            
+
+    def edit_wiki_new_page_edit_wo_login_test(self):    
+        subject = self.get_random_string(length=10)
+        opener = urllib2.build_opener(RedirectCatchHandler())
+        content = self.get_random_string()
+        test_url = base_url+wiki_path+edit_path+"/%s"%subject
+        if settings.DEBUG:
+            print "Test url " + test_url
+        page = opener.open(test_url,
+                "subject=%s&content=%s"%(subject, content))
+        self.assertEqual(login_url, page.headers.dict['location'])
+        if settings.DEBUG:
+            print "redirct to %s"%(page.geturl())
+
